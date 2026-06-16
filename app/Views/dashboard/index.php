5320c8654fbf83cb17ec2bb656ce7cd8ba201300
univille <univille@LAB-SIS-NOTE-02.univille.br>
2026-06-16 19:40:52 -0300

    Aula 03: login, sessão e proteção de páginas

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -1,17 +1,44 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" 
-        content="width=device-width, initial-scale=1.0">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard - AtendeLab</title>
 
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" 
-        rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body class="bg-light">
-    
+
+    <nav class="navbar  navbar-dark bg-dark">
+        <div class="container">
+            <span class="navbar-brand mb-0 h1">AtendeLab</span>
+            <a class="btn btn-sm btn-outline-light" href="?controller=auth&action=logout">Sair</a>
+        </div>
+    </nav>
+
+    <div class="container mt-4">
+        <div class="card shadow-sm">
+            <div class="card-body">
+                <h1 class="h4">Área Restrita</h1>
+                <p class="mb-1">Bem vindo
+
+                    <strong>
+                        <?= htmlspecialchars($usuario['nome'] ?? '', ENT_QUOTES, 'UTF-8') ?>
+                    </strong>
+                </p>
+                <p class="text-muted">
+                    Perfil:
+                    <?= htmlspecialchars($usuario['perfil'] ?? '', ENT_QUOTES, 'UTF-8') ?>
+                </p>
+
+                <a href="?controller=usuarios&action=listar" class="btn btn-primary">Testar rota Protegida de
+                    usuários</a>
+            </div>
+        </div>
+    </div>
+
 </body>
+
 </html>
